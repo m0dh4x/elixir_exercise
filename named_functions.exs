2a9c64ee68501fc218dd3b7daed29680c55d758a
Florian Drews <info@florian-drews.de>
2019-09-24 13:50:47 +0200

    excercise 4 and 5 modules and functions

--- named_functions.exs
+++ named_functions.exs
@@ -24,3 +24,8 @@ defmodule SumNaturals do
   def sum(0), do: 0
   def sum(n), do: n + sum(n - 1)
 end
+
+defmodule GreatestCommonDivisor do
+  def gcd(x,0), do: x
+  def gcd(x,y), do: gcd(y, rem(x,y))
+end
