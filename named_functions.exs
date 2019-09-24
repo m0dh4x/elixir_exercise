a67d35de4e3493ee630f1a36fa79e27abc2338d8
Florian Drews <info@florian-drews.de>
2019-09-24 09:56:25 +0200

    add short and multiple lines to do:

--- named_functions.exs
+++ named_functions.exs
@@ -2,4 +2,12 @@ defmodule Times do
   def double(n) do
     n * 2
   end
+  def doubleShort(n), do: n * 2
+end
+
+defmodule Greetings do
+  def greeting(greeting, name), do: (
+    IO.puts greeting
+    IO.puts "How are you #{name}?"
+  )
 end
