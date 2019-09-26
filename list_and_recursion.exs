bc9337e9677a16944909932d2309074fd6a79ad2
Florian Drews <info@florian-drews.de>
2019-09-26 09:46:40 +0200

    List with functions like map

--- list_and_recursion.exs
+++ list_and_recursion.exs
@@ -1,4 +1,14 @@
 defmodule MyList do
   def len([]), do: 0
-  def len([head|tail]), do: 1 + len(tail)
+  def len([_head|tail]), do: 1 + len(tail)
+end
+
+defmodule MyListSquare do
+  def square([]), do: []
+  def square([head|tail]), do: [ head * head | square(tail) ]
+end
+
+defmodule MyListWithFunc do
+  def map([], _func), do: []
+  def map([head|tail], func), do: [ func.(head) | map(tail, func) ]
 end
