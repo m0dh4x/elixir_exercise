b1bf9ff2a654f7a98529daabd5adc708b2c94193
Florian Drews <info@florian-drews.de>
2019-09-27 15:23:55 +0200

    refactoring InspectMap

--- maps_lists_keywords.exs
+++ maps_lists_keywords.exs
@@ -10,3 +10,17 @@ defmodule Canvas do
     IO.puts "Style #{inspect Keyword.get_values(options, :style)}"
   end
 end
+
+defmodule InspectMaps do
+  @people [
+    %{ name: "Grumpy",    height: 1.24 },
+    %{ name: "Dave",      height: 1.88 },
+    %{ name: "Dopey",     height: 1.32 },
+    %{ name: "Shaquille", height: 2.16 },
+    %{ name: "Sneezy",    height: 1.28 }
+  ]
+  def inspect_map() do
+    for people = %{height: height} <- @people, height > 1.5, do: people.name
+  end
+end
+
